8fa496068fb6cc0186188f53164ae022bddbf511
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2024-11-20 15:04:11 +1100

    Loading in from DataBase
    Gives the ability to laod from the databse, although images do not load

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('.database/data_source.db');
+
 const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.listen(5000, () =>
-    console.log(
+  console.log(
     "Server is running on Port 5000, visit http://localhost:5000/ or http://127.0.0.1:5000 to access your website"
-    )
+  )
 );
+
+app.get('/api/fans', (req, res) =>{
+    db.all('SELECT * FROM FansOnly', [], (err,rows) =>{
+        // * successful Queer
+        res.json({fans : rows}); 
+    })
+})
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,31 @@
-async function loadStuff(){
-    let result = "";
-    fetch("./frontEndData.json")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        appendData(data);
-      })
-      .catch(function (err) {
-        console.log("error: " + err);
-      });
-}
+let result = "";
+
+fetch("/api/fans")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data.fans);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
+
 
 function appendData(data) {
-    data.forEach(({ name, type, smallInfo, description, image } = rows) => {
-    result += `
-        <div class="fanContent">
-            <img src="${image}" alt="Your internet wasn't a big fan" class="fanImage">
-            <div class="fanDetail">
-                <h1 class="mainTextH1">${name}</h1>
-                <p class="mainTextFanType">${smallInfo}</p>
-                <p class="mainTextFanInfo">${description}</p>
-            </div>
+data.forEach(({ name, smallInfo, description, image} = rows) => {
+result += `
+    <div class="fanContent">
+        <img src="${image}" alt="Your internet wasn't a big fan" class="fanImage">
+        <div class="fanDetail">
+            <h1 class="mainTextH1">${name}</h1>
+            <p class="mainTextFanType">${smallInfo}</p>
+            <p class="mainTextFanInfo">${description}</p>
         </div>
-        `;
-    });
-    document.querySelector(".container").innerHTML = result;
-    }
+    </div>
+    `;
+});
+document.querySelector(".mainText").innerHTML = result;
+}
 
 window.onLoad = loadStuff;
