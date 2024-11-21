eb63d01a13353dcc6f7868e75a3763c97b8b82bd
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2024-11-21 12:36:42 +1100

    Small changes, with correct first data

--- public/js/app.js
+++ public/js/app.js
@@ -13,19 +13,27 @@ fetch("/api/fans")
 
 
 function appendData(data) {
-data.forEach(({ name, smallInfo, description, image} = rows) => {
-result += `
-    <div class="fanContent">
-        <img src="${image}" alt="Your internet wasn't a big fan" class="fanImage">
-        <div class="fanDetail">
-            <h1 class="mainTextH1">${name}</h1>
-            <p class="mainTextFanType">${smallInfo}</p>
-            <p class="mainTextFanInfo">${description}</p>
-        </div>
-    </div>
-    `;
-});
-document.querySelector(".mainText").innerHTML = result;
+  data.forEach(({ name, type, smallInfo, description, image} = rows) => {
+    var newType = "";
+    if(type == 1){
+      newType = "Premium";
+    }
+    else{
+      newType = "Standard";
+    }
+  result += `
+      <div class="fanContent">
+          <img src="${image}" alt="Your internet wasn't a big fan" class="fanImage">
+          <div class="fanDetail">
+              <h1 class="mainTextH1">${name}</h1>
+              <p class="mainTextFanType">${smallInfo}</p>
+              <p class="mainTextFanType">${newType}</p>
+              <p class="mainTextFanInfo">${description}</p>
+          </div>
+      </div>
+      `;
+  });
+  document.querySelector(".mainText").innerHTML = result;
 }
 
 window.onLoad = loadStuff;
