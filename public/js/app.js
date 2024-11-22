427d696ba9133e12a504e85a372038446c8edca7
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2024-11-22 18:44:47 +1100

    Main page ranndomization
    The main page is randomized and will generate a random photo and information for the database.

--- public/js/app.js
+++ public/js/app.js
@@ -13,27 +13,53 @@ fetch("/api/fans")
 
 
 function appendData(data) {
-  data.forEach(({ name, type, smallInfo, description, image} = rows) => {
-    var newType = "";
-    if(type == 1){
-      newType = "Premium";
-    }
-    else{
-      newType = "Standard";
-    }
+  var randNum = getRandomInt(data.length);
+
+  var newType = "";
+  if(data[randNum].type == 1){
+    newType = "Premium";
+  }
+  else{
+    newType = "Standard";
+  }
+
   result += `
-      <div class="fanContent">
-          <img src="${image}" alt="Your internet wasn't a big fan" class="fanImage">
-          <div class="fanDetail">
-              <h1 class="mainTextH1">${name}</h1>
-              <p class="mainTextFanType">${smallInfo}</p>
-              <p class="mainTextFanType">${newType}</p>
-              <p class="mainTextFanInfo">${description}</p>
-          </div>
+  <div class="fanContent">
+      <img src="${data[randNum].image}" alt="Your internet wasn't a big fan" class="fanImage">
+      <div class="fanDetail">
+          <h1 class="mainTextH1">${data[randNum].name}</h1>
+          <p class="mainTextFanType">${data[randNum].smallInfo}</p>
+          <p class="mainTextFanType">${newType}</p>
+          <p class="mainTextFanInfo">${data[randNum].description}</p>
       </div>
-      `;
-  });
+  </div>
+  `;
+
+  // data.forEach(({ name, type, smallInfo, description, image} = rows) => {
+  //   var newType = "";
+  //   if(type == 1){
+  //     newType = "Premium";
+  //   }
+  //   else{
+  //     newType = "Standard";
+  //   }
+  // result += `
+  //     <div class="fanContent">
+  //         <img src="${image}" alt="Your internet wasn't a big fan" class="fanImage">
+  //         <div class="fanDetail">
+  //             <h1 class="mainTextH1">${name}</h1>
+  //             <p class="mainTextFanType">${smallInfo}</p>
+  //             <p class="mainTextFanType">${newType}</p>
+  //             <p class="mainTextFanInfo">${description}</p>
+  //         </div>
+  //     </div>
+  //     `;
+  // });
   document.querySelector(".mainText").innerHTML = result;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 window.onLoad = loadStuff;
