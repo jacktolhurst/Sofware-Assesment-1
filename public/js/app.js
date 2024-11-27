c37a6af485b377a6d81b77afd165ea2cd14f92ec
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2024-11-27 14:41:48 +1100

    Fully made AllFans Page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 let result = "";
+let resultArray = [];
 
 fetch("/api/fans")
   .then(function (response) {
@@ -13,33 +14,103 @@ fetch("/api/fans")
 
 
 function appendData(data) {
-  var num = CalcRandomByDay(data.length);
+  var page = "";
+  if(document.URL.includes("PremiumFans")){
+    page = "PremiumFans";
+  }
+  else if(document.URL.includes("AllFans")){
+    page = "AllFans";
+  }
+  else if(document.URL.includes("index")){
+    page = "index";
+  }
 
+  var logoLink = "../images/TEMP/FanIconTemp.png";
   var newType = "";
-  if(data[num].type == 1){
-    newType = "Premium";
-  }
-  else if(data[num].type == 2){
-    newType = "Deformed";
-  }
-  else{
-    newType = "Standard";
+
+  if(page == "PremiumFans"){
+    data.forEach(({ name, type, smallInfo, description, image, link} = rows) => {
+      if(type == 1){
+        newType = "Premium";
+        var logoLink = "../images/TEMP/FanIconTemp.png";
+        
+        result +=`
+          <a id="premiumLink" href="${link}" target="_blank">
+            <div class="premiumFanContent">
+              <img class="PremiumFanImageMain" src="${logoLink}" alt="">
+              <img class="PremiumFanImage" src="${"../" + image}" alt="">
+              <h1 class="PremiumFanHeader">${name}</h1>
+              <p class="PremiumFanType" id="${newType}">Premium</p>
+              <p class="PremiumFanText">${smallInfo + " __ " + description}</p>
+            </div>
+          </a>
+        `
+      }
+    });
+
+    document.querySelector(".premiumFanContainer").innerHTML = result;
   }
 
-  result += `
-  <div class="fanContent">
-      <img src="${data[num].image}" alt="Your internet wasn't a big fan" class="fanImage">
-      <div class="fanDetail">
-          <h1 class="mainTextH1">${data[num].name}</h1>
-          <p class="mainTextFanType">${data[num].smallInfo}</p>
-          <p class="mainTextFanType" id="${newType}"=>${newType}</p>
-          <p class="mainTextFanInfo">${data[num].description}</p>
-          <a href="${data[num].link}" target="_blank">Find The Fan</a>
-      </div>
-  </div>
-  `;
+  if(page == "AllFans"){
+    data.forEach(({ name, type, smallInfo, description, image, link} = rows) => {
+        if(type == 1){
+          newType = "Premium";
+        }
+        else if(type == 2){
+          newType = "Deformed";
+        }
+        else{
+          newType = "Standard";
+        }
+        result +=`
+          <a id="premiumLink" href="${link}" target="_blank">
+            <div class="premiumFanContent">
+              <img class="PremiumFanImageMain" src="${logoLink}" alt="">
+              <img class="PremiumFanImage" src="${"../" + image}" alt="">
+              <h1 class="PremiumFanHeader">${name}</h1>
+              <p class="PremiumFanType" id="${newType}">${newType}</p>
+              <p class="PremiumFanText">${smallInfo + " __ " + description}</p>
+            </div>
+          </a>
+        `
+    });
 
-  document.querySelector(".mainText").innerHTML = result;
+    console.log(result);
+
+    document.querySelector(".premiumFanContainer").innerHTML = result;
+  }
+  
+  if(page == "index"){
+    var num = CalcRandomByDay(data.length);
+
+    if(data[num].type == 1){
+      newType = "Premium";
+    }
+    else if(data[num].type == 2){
+      newType = "Deformed";
+    }
+    else{
+      newType = "Standard";
+    }
+
+    result += `
+    <div class="fanContent">
+        <a href="${data[num].link}" target="_blank">
+          <img src="${data[num].image}" alt="Your internet wasn't a big fan" class="fanImage">
+        </a>
+        <div class="fanDetail">
+            <h1 class="mainTextH1">${data[num].name}</h1>
+            <p class="mainTextFanType">${data[num].smallInfo}</p>
+            <p class="mainTextFanType" id="${newType}">${newType}</p>
+            <p class="mainTextFanInfo">${data[num].description}</p>
+            <a href="${data[num].link}" target="_blank">Find The Fan</a>
+        </div>
+    </div>
+    `;
+
+    document.querySelector(".mainText").innerHTML = result;
+  }
+
 }
 
 function CalcRandomByDay(max) {
@@ -60,5 +131,3 @@ if ("serviceworker" in navigator) {
       .catch((err) => console.log("service worker not registered", err));
   });
 }
-
-window.onLoad = loadStuff;
